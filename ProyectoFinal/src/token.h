f0cbbb6d8319bf699d30ca58328231a97706007e
SaulRecio06 <76199088+SaulRecio06@users.noreply.github.com>
2023-07-25 10:05:54 -0700

    Update token.h

--- ProyectoFinal/src/token.h
+++ ProyectoFinal/src/token.h
@@ -1,11 +1,9 @@
 // Reemplazar con tus credenciales de Wifi
-// const char* ssid     = "Totalplay-82A3";
-// const char* password = "82A3DC71ArDj6abQ";
-const char* ssid     = "NokiaSaul";
-const char* password = "saul.123";
+const char* ssid     = "******";
+const char* password = "******";
 const int led1 = 12;
 
 
 // Inicializa Bot Telegram
-#define BOTtoken "5765981680:AAHGHG01GO-LfKpA_LDzZvF7o6RIj0QUOKU"  // Tu Bot Token (Obtener de Botfather)
-#define CHAT_ID "1396897208"
+#define BOTtoken "*******"  // Tu Bot Token (Obtener de Botfather)
+#define CHAT_ID "******"
